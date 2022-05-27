b2c9e21bb359f002350ec4ba9bdc32f513431c5d
Patrick Hayo <patrick.hayo@email.de>
2022-05-27 10:25:54 +0200

    initial commit

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "identities" {
+  description = "identity configuration"
+  type = list(object({
+    name                = string
+    location            = string
+    resource_group_name = string
+    tags                = map(any)
+  }))
+}
+
+variable "tags" {
+  description = "(Optional) Specifies tags for all the resources"
+  default     = {}
+}
